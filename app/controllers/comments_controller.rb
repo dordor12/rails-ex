b79ea149e88896ef3d68d475c24b7ec5450079ae
Dor <doramid1590@gmail.com>
2020-10-22 21:43:34 +0300

    fix comment ontroller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+  skip_before_action :verify_authenticity_token
 
   http_basic_authenticate_with name: (ENV["APPLICATION_USER"].blank? ? 'test' : ENV['APPLICATION_USER']), password: (ENV["APPLICATION_PASSWORD"].blank? ? 'test' : ENV["APPLICATION_PASSWORD"]), only: :destroy
 
